59fdf81c6a104b1fee6297a1740bdb90d93244bf
Gustavo M. R. Santos <gustavo.rosendo@b-plat.net>
2022-04-30 12:58:59 -0300

    add user authentication functionality

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+  
+  def new
+    redirect_to user_path(current_user) if user_signed_in?
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+
+    # the authenticate fuction comes from the 'has_secure_password' option in the User model
+    if user && user.authenticate(params[:session][:password])
+      sign_in(user)
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = 'Email ou senha inválidos'
+      render 'new'
+    end
+  end
+
+  def destroy
+    sign_out
+    flash[:success] = 'Logout efetuado com sucesso'
+    redirect_to entrar_path
+  end
+
+end
